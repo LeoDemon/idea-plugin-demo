0db9b7e97181236f06a12436e0620335ca991924
Demon.Lee <jwkljh@aliyun.com>
2023-06-02 14:44:43 +0800

    first Action for MainToolBar

---
+++ src/main/java/tech/demonlee/ideaplugindemo/MyFirstAction.java
@@ -0,0 +1,13 @@
+package tech.demonlee.ideaplugindemo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+
+public class MyFirstAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Messages.showInfoMessage("Hi 你好 IDEA", "FirstAction");
+    }
+}

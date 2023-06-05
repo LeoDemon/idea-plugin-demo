8dd083e85f791cde7e8354ccea28e979f878f5ee
Demon.Lee <jwkljh@aliyun.com>
2023-06-05 10:42:23 +0800

    feat(Action): add new file info action for FileMenu

---
+++ src/main/java/tech/demonlee/ideaplugindemo/FileInfoAction.java
@@ -0,0 +1,26 @@
+package tech.demonlee.ideaplugindemo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Demon.Lee
+ * @date 2023-06-05 10:02
+ */
+public class FileInfoAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+        String classPath = psiFile.getVirtualFile().getPath();
+        Messages.showMessageDialog(project, "FileInfo Plugin: " + classPath,
+                "你好，IDEA 插件", Messages.getInformationIcon());
+    }
+}
